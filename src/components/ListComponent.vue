54893a4e774e03a9114c2d85b61420d2b924fb93
abergavenny <abergavenny@protonmail.ch>
2022-09-26 18:13:18 +0200

    Initial template

---
+++ src/components/ListComponent.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+defineProps(["data"]);
+</script>
+
+<template>
+  <v-container>
+    <div v-for="element in data" :key="element"></div>
+  </v-container>
+</template>
---
+++ src/views/MainView.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import CustomComponent from "@/components/CustomComponent.vue";
+</script>
+
+<template>
+  <CustomComponent></CustomComponent>
+</template>
